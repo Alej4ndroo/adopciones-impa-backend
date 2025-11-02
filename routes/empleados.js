1b682cc6ce9ed296777f674539b76180daed073f
Alej4ndroo <alexlopezchaparro@gmail.com>
2025-11-02 01:54:15 -0600

    Subir backend de Adopciones IMPA

--- routes/empleados.js
+++ routes/empleados.js
@@ -7,7 +7,7 @@ const { verifyToken, checkPermission } = require('../middlewares/authMiddleware'
 // Ruta para API
 router.get('/listar', verifyToken, checkPermission('ver_empleado'), empleadosController.listar);
 router.get('/obtener-perfil', verifyToken, empleadosController.obtener_perfil);
-router.post('/actualizar', verifyToken, checkPermission('editar_empleado'), empleadosController.actualizar);
+//router.post('/actualizar', verifyToken, checkPermission('editar_empleado'), empleadosController.actualizar);
 router.post('/crear', verifyToken, checkPermission('crear_empleado'), empleadosController.crear);
 
 module.exports = router;
